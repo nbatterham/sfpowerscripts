a30b1401d6b760aa754f2c7ffec5a1b407524471
azlam-abdulsalam <azlam.abdulsalam@accenture.com>
2019-09-02 13:43:37 +1000

    Fixed Path Issues

--- Tests/IncrementProjectBuildNumberTask/IncrementProjectBuildNumberTest.cmd
+++ Tests/IncrementProjectBuildNumberTask/IncrementProjectBuildNumberTest.cmd
@@ -1,6 +1,6 @@
 SET INPUT_SEGMENT=Patch
-SET INPUT_PACKAGE=
-SET INPUT_PROJECT_DIRECTORY=C:\Projects\racv_salesforce\mex_salesforce_customer
+SET INPUT_PACKAGE=customer
+SET INPUT_PROJECT_DIRECTORY=
 
 
 
