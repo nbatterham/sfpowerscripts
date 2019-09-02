46e000942c81b0729f7fe617b5606250ada3ba95
azlam-abdulsalam <azlam.abdulsalam@accenture.com>
2019-09-02 17:01:25 +1000

    Created Unlocked package

---
+++ BuildTasks/CreateUnlockedPackageTask/CreateUnlockedPackageImpl.ts
@@ -0,0 +1,55 @@
+import child_process = require("child_process");
+import { isNullOrUndefined } from "util";
+import { onExit } from "../Common/OnExit";
+
+export default class CreateUnlockedPackageImpl {
+  public constructor(
+    private sfdx_package: string,
+    private version_number: string,
+    private tag: string,
+    private config_file_path: string,
+    private installationkeybypass: boolean,
+    private installationkey: string,
+    private project_directory: string,
+    private devhub_alias: string,
+    private wait_time: string
+  ) {}
+
+  public async exec(command: string): Promise<string> {
+
+    let child=child_process.exec(command,  { encoding: "utf8" },(error,stdout,stderr)=>{
+
+      if(error)
+         throw error;
+    });
+   
+  
+    let output="";
+    child.stdout.on("data",data=>{console.log(  data.toString()); 
+      output+=data.toString();
+    });
+
+   let result = JSON.parse(output);
+
+    await onExit(child);
+
+    return result.result.SubscriberPackageVersionId;
+
+  }
+
+  public async buildExecCommand(): Promise<string> {
+    let command = `npx sfdx force:package:version:create -p ${this.sfdx_package}  -w ${this.wait_time} --definitionfile ${this.config_file_path} --versionnumber ${this.version_number }`
+
+    if(this.installationkeybypass)
+     command+=` -x`
+    else
+     command+=` -k ${this.installationkey}`
+    
+    if(isNullOrUndefined(this.tag))
+    command+=` -t ${this.tag}`
+
+    command+=` -v ${this.devhub_alias}`
+
+    return command;
+  }
+}
