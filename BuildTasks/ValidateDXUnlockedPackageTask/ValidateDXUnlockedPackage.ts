a71d0b32c26f4320ae7ecd0e4608befab2e7adbe
azlamsalam <azlam.salam@outlook.com>
2019-10-04 16:14:22 +1000

    update validate dx unlocked package

--- BuildTasks/ValidateDXUnlockedPackageTask/ValidateDXUnlockedPackage.ts
+++ BuildTasks/ValidateDXUnlockedPackageTask/ValidateDXUnlockedPackage.ts
@@ -5,9 +5,10 @@ async function run() {
   try {
     const validate_package: string = tl.getInput("package", false);
     const project_directory: string = tl.getInput("working_directory", false);
+    const bypass: string = tl.getInput("bypass", false);
     
     
-    let validateApexCoverageImpl:ValidateDXUnlockedPackageImpl = new ValidateDXUnlockedPackageImpl(validate_package,project_directory);
+    let validateApexCoverageImpl:ValidateDXUnlockedPackageImpl = new ValidateDXUnlockedPackageImpl(validate_package,bypass,project_directory);
     console.log("Generating command");
     let command = await validateApexCoverageImpl.buildExecCommand();
     tl.debug(command);
--- BuildTasks/ValidateDXUnlockedPackageTask/ValidateDXUnlockedPackageImpl.ts
+++ BuildTasks/ValidateDXUnlockedPackageTask/ValidateDXUnlockedPackageImpl.ts
@@ -1,9 +1,10 @@
 import child_process = require("child_process");
 import { onExit } from "../Common/OnExit";
+import { isNullOrUndefined } from "util";
 
 
 export default class ValidateDXUnlockedPackageImpl {
-  public constructor(private validate_package: string, private project_directory: string) {}
+  public constructor(private validate_package: string,private bypass:string, private project_directory: string) {}
 
   public async exec(command: string): Promise<void> {
    
@@ -24,10 +25,15 @@ export default class ValidateDXUnlockedPackageImpl {
   public async buildExecCommand(): Promise<string> {
 
     let command;
-    if(this.validate_package!=null && this.validate_package.length>0)
-    command = `npx sfdx sfpowerkit:package:valid -n  ${this.validate_package}`;
-    else
-    command = `npx sfdx sfpowerkit:package:valid`;
+        command = `npx sfdx sfpowerkit:package:valid`;
+
+
+    if(!isNullOrUndefined(this.validate_package))
+    command+=` -n  ${this.validate_package}`;
+
+    if(!isNullOrUndefined(this.bypass))
+    command+=` -b  ${this.bypass}`;
+
 
     return command;
   }
