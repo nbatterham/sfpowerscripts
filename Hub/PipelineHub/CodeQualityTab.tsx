ceccc5d958014f1e09bbd0f998ecc3f009954fa0
azlamsalam <azlam.salam@outlook.com>
2019-12-03 00:52:24 +1100

    Added Header

--- Hub/PipelineHub/CodeQualityTab.tsx
+++ Hub/PipelineHub/CodeQualityTab.tsx
@@ -4,24 +4,63 @@ import * as SDK from "azure-devops-extension-sdk";
 import { Button } from "azure-devops-ui/Button";
 import { TextField } from "azure-devops-ui/TextField";
 import { Card } from "azure-devops-ui/Card";
+import { Header, TitleSize } from "azure-devops-ui/Header";
+import { IHeaderCommandBarItem } from "azure-devops-ui/HeaderCommandBar";
 
 const stats = [
-    {
-        label: "Minor Bugs",
-        value: 20
+  {
+    label: "Code Coverage",
+    value: 68,
+    trent : 1
+  },
+  {
+    label: "Minor Bugs",
+    value: 20,
+    trent : 1
+  },
+  {
+    label: "Major",
+    value: 3,
+    trent : 1
+  },
+  {
+    label: "Critical",
+    value: 5,
+    trent : -1
+  }
+];
+
+const commandBarItemsSimple: IHeaderCommandBarItem[] = [
+  {
+    iconProps: {
+      iconName: "Add"
     },
-    {
-        label: "Major",
-        value: 3
+    id: "addProject",
+    important: true,
+    onActivate: () => {
+
+      //How to add a new project?  
+      alert("This would normally trigger a modal popup");
     },
-    {
-        label: "Critical",
-        value: 5
+    text: "Action",
+    tooltipProps: {
+      text: "Custom tooltip for create"
     }
+  },
+  {
+    iconProps: {
+      iconName: "Delete"
+    },
+    id: "deleteProject",
+    important: false,
+    onActivate: () => {
+      alert("submenu clicked");
+    },
+    text: "Menu row with delete icon"
+  }
 ];
 
 export class CodeQualityTab extends React.Component<{}> {
- 
   constructor(props: {}) {
     super(props);
     this.state = {};
@@ -35,20 +74,24 @@ export class CodeQualityTab extends React.Component<{}> {
     await SDK.ready();
     const accessToken = await SDK.getAccessToken();
 
-   
-
     //Magic here
   }
 
   public render(): JSX.Element {
-
-
     //https://developer.microsoft.com/en-us/azure-devops/components/card#advanced-card
 
     return (
       <div className="page-content page-content-top flex-row rhythm-horizontal-16">
+        <Header
+          title={"Projects"}
+          commandBarItems={commandBarItemsSimple}
+          titleSize={TitleSize.Medium}
+          titleIconProps={{ iconName: "OpenSource" }}
+        />
 
 
+        // Need to create cards as much as we can 
+        
         <Card
           className="flex-grow"
           titleProps={{ text: "Code Analysis : <<Inspections>>" }}
