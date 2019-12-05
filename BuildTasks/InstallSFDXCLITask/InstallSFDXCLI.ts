85dd649ba304f44097ca61cfbf2c3c82f4842629
azlamsalam <azlam.salam@outlook.com>
2019-12-05 12:27:55 +1100

    Add telemetry to deprecated tasks

--- BuildTasks/InstallSFDXCLITask/InstallSFDXCLI.ts
+++ BuildTasks/InstallSFDXCLITask/InstallSFDXCLI.ts
@@ -1,11 +1,14 @@
 import tl = require('azure-pipelines-task-lib/task');
 import child_process = require('child_process');
+import { AppInsights } from "../Common/AppInsights";
 
 
 async function run() {
     try {
       
         console.log("SFPowerScript.. Install SFDX/SFPowerkit")
+
+        AppInsights.setupAppInsights(true);
 
         const cli_version: string = tl.getInput('sfdx_cli_version', false);
         const sfpowerkit_version: string = tl.getInput('sfpowerkit_version', false);
@@ -14,7 +17,9 @@ async function run() {
         child_process.execSync(`sudo yarn global add sfdx-cli@${cli_version}`);
         child_process.execSync(`echo 'y' | npx sfdx plugins:install sfpowerkit@${sfpowerkit_version}`);
 
-        console.log("SFDX along with SFPowerkit installed succesfully")
+        console.log("SFDX along with SFPowerkit installed succesfully");
+        AppInsights.trackTask("sfpwowerscript-installsfdx-old-task");
+        AppInsights.trackTaskEvent("sfpwowerscript-installsfdx-old-task");
     }
     catch (err) {
         tl.setResult(tl.TaskResult.Failed, err.message);
--- BuildTasks/ManageScratchOrgTask/ManageScratchOrg.ts
+++ BuildTasks/ManageScratchOrgTask/ManageScratchOrg.ts
@@ -3,68 +3,84 @@ import child_process = require("child_process");
 import DeleteScratchOrgImpl from "./DeleteScratchOrgImpl";
 import CreateScratchOrgImpl from "./CreateScratchOrgImpl";
 
+import { AppInsights } from "../Common/AppInsights";
 
 async function run() {
   try {
-    
-
-
-    const action:string = tl.getInput("action",true);
+    const action: string = tl.getInput("action", true);
     const devhub_alias: string = tl.getInput("devhub_alias", true);
-   
 
-    if(action == "Create")
-    {
-    console.log("SFPowerScript.. Create a scratch org");
-    const alias: string = tl.getInput("alias", true);
-    const config_file_path: string = tl.getInput("config_file_path", true);
-    const working_directory: string = tl.getInput("working_directory", false);
+    AppInsights.setupAppInsights(true);
 
+    if (action == "Create") {
+      console.log("SFPowerScript.. Create a scratch org");
+      const alias: string = tl.getInput("alias", true);
+      const config_file_path: string = tl.getInput("config_file_path", true);
+      const working_directory: string = tl.getInput("working_directory", false);
 
-    let createScratchOrg:CreateScratchOrgImpl = new CreateScratchOrgImpl(working_directory,config_file_path,devhub_alias,alias);
-    console.log("Generating Create Scratch Org command");
-    let createCommand = await createScratchOrg.buildExecCommand();
-    tl.debug(createCommand);
-   
-    let result = await createScratchOrg.exec(createCommand);
+      let createScratchOrg: CreateScratchOrgImpl = new CreateScratchOrgImpl(
+        working_directory,
+        config_file_path,
+        devhub_alias,
+        alias
+      );
+      console.log("Generating Create Scratch Org command");
+      let createCommand = await createScratchOrg.buildExecCommand();
+      tl.debug(createCommand);
 
-    tl.setTaskVariable('sfpowerscripts_scratch_org_username',result.result.username,false);
-    
-    console.log(
-      `Successfully created a scratch org with devhub ${devhub_alias} , The username  is ${result.result.username}`
-    );
+      let result = await createScratchOrg.exec(createCommand);
 
-    //Open up the scratch org to get the URL
-    console.log(`Opening up the created scratch org to retrieve URL`)
-    result = child_process.execSync(`npx sfdx force:org:open -r --json`, { cwd: working_directory, encoding: "utf8" });
-    tl.debug(result);
-    let resultAsJSON=JSON.parse(result);
-    tl.setTaskVariable('sfpowerscripts_scratch_org_url',resultAsJSON.result.url,false);
-    
-    console.log(
-      `Successfully retrieved scratch org url ${resultAsJSON.result.url}`
-    );
-   
+      tl.setTaskVariable(
+        "sfpowerscripts_scratch_org_username",
+        result.result.username,
+        false
+      );
 
-    }
-    else
-    {
+      console.log(
+        `Successfully created a scratch org with devhub ${devhub_alias} , The username  is ${result.result.username}`
+      );
+
+      //Open up the scratch org to get the URL
+      console.log(`Opening up the created scratch org to retrieve URL`);
+      result = child_process.execSync(`npx sfdx force:org:open -r --json`, {
+        cwd: working_directory,
+        encoding: "utf8"
+      });
+      tl.debug(result);
+      let resultAsJSON = JSON.parse(result);
+      tl.setTaskVariable(
+        "sfpowerscripts_scratch_org_url",
+        resultAsJSON.result.url,
+        false
+      );
+
+      console.log(
+        `Successfully retrieved scratch org url ${resultAsJSON.result.url}`
+      );
+
+      AppInsights.trackTaskEvent(
+        "sfpwowerscript-managescratchorg-old-task",
+        "scratchorg_created"
+      );
+    } else {
       console.log("SFPowerScript.. Delete a scratch org");
-
 
       const target_org: string = tl.getInput("target_org", true);
 
-      let deleteScratchOrgImpl:DeleteScratchOrgImpl = new DeleteScratchOrgImpl(target_org,devhub_alias);
+      let deleteScratchOrgImpl: DeleteScratchOrgImpl = new DeleteScratchOrgImpl(
+        target_org,
+        devhub_alias
+      );
       console.log("Generating Delete Scratch Org command");
       let command = await deleteScratchOrgImpl.buildExecCommand();
       tl.debug(command);
       await deleteScratchOrgImpl.exec(command);
 
-      
+      AppInsights.trackTaskEvent(
+        "sfpwowerscript-managescratchorg-old-task",
+        "scratchorg_deleted"
+      );
     }
-
-   
-   
   } catch (err) {
     tl.setResult(tl.TaskResult.Failed, err.message);
   }
