c24481ca5ac70f4ca61ea4729b09304ac024c406
azlamsalam <azlam.salam@outlook.com>
2019-12-07 00:54:05 +1100

    Removed Tag Code, Added Service Connection

--- BuildTasks/CheckoutProjectFromArtifactTask/CheckoutProjectFromArtifact.ts
+++ BuildTasks/CheckoutProjectFromArtifactTask/CheckoutProjectFromArtifact.ts
@@ -2,80 +2,104 @@ import tl = require("azure-pipelines-task-lib/task");
 import child_process = require("child_process");
 var fs = require("fs");
 const path = require("path");
-import  simplegit from 'simple-git/promise';
+import simplegit from "simple-git/promise";
 import { AppInsights } from "../Common/AppInsights";
-
-
-import rimraf = require("rimraf");
 
 
 async function run() {
   try {
+    const artifact = tl.getInput("artifact", true);
+    const version_control_provider: string = tl.getInput(
+      "version_control_provider",
+      true
+    );
 
+    let connection: string;
+    switch (version_control_provider) {
+      case "github":
+        connection = tl.getInput("github_connection", true);
+        break;
+      case "github_enterprise":
+        connection = tl.getInput("github_enterprise_connection", true);
+        break;
+      case "bitbucket":
+        connection = tl.getInput("bitbucket", true);
+        break;
+      case "other_git":
+        connection = tl.getInput("other_git_connection", true);
+        break;
+    }
 
-    const username = tl.getInput("username",true);
-    const password = tl.getInput("password",true);
-
-    const artifact =tl.getInput("artifact",true);
-
-    
+    let token;
+    if (version_control_provider == "azure_repo") {
+      token = tl.getVariable("system.accessToken");
+    } else {
+      token = tl.getEndpointAuthorizationParameter(
+        connection,
+        "AccessToken",
+        true
+      );
+    }
 
     let artifact_directory = tl.getVariable("system.artifactsDirectory");
 
-    AppInsights.setupAppInsights(tl.getBoolInput("isTelemetryEnabled",true));
+    AppInsights.setupAppInsights(tl.getBoolInput("isTelemetryEnabled", true));
 
-      let package_version_id_file_path = path.join(
-        artifact_directory,
-        artifact,
-        "sfpowerkit_artifact",
-        "artifact_metadata"
-      );
+    let package_version_id_file_path = path.join(
+      artifact_directory,
+      artifact,
+      "sfpowerkit_artifact",
+      "artifact_metadata"
+    );
 
-     let package_metadata_json = fs
-        .readFileSync(package_version_id_file_path)
-        .toString();
+    let package_metadata_json = fs
+      .readFileSync(package_version_id_file_path)
+      .toString();
 
+    let package_metadata = JSON.parse(package_metadata_json);
+    let local_source_directory = path.join(
+      artifact_directory,
+      artifact,
+      "source"
+    );
 
+    fs.mkdirSync(local_source_directory, { recursive: true });
 
-   let package_metadata = JSON.parse(package_metadata_json);
-   let local_source_directory=path.join(
-    artifact_directory,
-    artifact,
-    "source"
-  );
+    //Strinp https
+    const removeHttps = input => input.replace(/^https?:\/\//, "");
 
+    package_metadata.repository_url = removeHttps(
+      package_metadata.repository_url
+    );
 
-  fs.mkdirSync(local_source_directory,{ recursive: true });
+    console.log(package_metadata);
 
-  //Strinp https
-  const removeHttps = input => input.replace(/^https?:\/\//, '');
+    let remote: string;
+    if (version_control_provider == "bitbucket") {
+       remote = `https://x-token-auth:${token}@${package_metadata.repository_url}.git`;
+    } else  {
+       remote = `https://${token}@${package_metadata.repository_url}.git`;
+    }
 
-  package_metadata.repository_url = removeHttps( package_metadata.repository_url);
+    const git = simplegit(local_source_directory);
+    await git.silent(false).clone(remote, local_source_directory);
+    await git.checkout(package_metadata.sourceVersion);
 
-   console.log(package_metadata);
-  
-   const remote = `https://${username}:${password}@${package_metadata.repository_url}.git`;
+    console.log(`Checked Out ${package_metadata.sourceVersion} sucessfully`);
 
+    fs.readdirSync(local_source_directory).forEach(file => {
+      console.log(file);
+    });
 
-   const git =  simplegit(local_source_directory);
-   await git.silent(false).clone(remote,local_source_directory);  
-   await git.checkout(package_metadata.sourceVersion);
+    tl.setVariable("sfpowerscripts_checked_out_path", local_source_directory);
 
-   console.log(`Checked Out ${package_metadata.sourceVersion} sucessfully`);
-
-   fs.readdirSync(local_source_directory).forEach(file => {
-    console.log(file);
-  });
-
- 
-  tl.setVariable("sfpowerscripts_checked_out_path", local_source_directory);
-
-  AppInsights.trackTask("sfpwowerscript-checkoutprojectfromartifact-task");
-  AppInsights.trackTaskEvent("sfpwowerscript-checkoutprojectfromartifact-task","project_checked_out");       
-
-   
+    AppInsights.trackTask("sfpwowerscript-checkoutprojectfromartifact-task");
+    AppInsights.trackTaskEvent(
+      "sfpwowerscript-checkoutprojectfromartifact-task",
+      "project_checked_out"
+    );
   } catch (err) {
-    AppInsights.trackExcepiton("sfpwowerscripts-createsourcepackage-task",err);
+    AppInsights.trackExcepiton("sfpwowerscripts-createsourcepackage-task", err);
     tl.setResult(tl.TaskResult.Failed, err.message);
   }
 }
